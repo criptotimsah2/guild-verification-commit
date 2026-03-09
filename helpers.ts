0a92938a8ac30f73b68bca9207a0a06977763960
criptotimsah2 <criptotimsah2@gmail.com>
2026-03-09 14:07:21 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T10:29:45.168Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-09T11:07:21.538Z
+Commit: Resolve timezone issues in date handling
