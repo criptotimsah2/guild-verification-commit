1c3aa2bb19884def4f701ed9da5b2e2e054c80ed
criptotimsah2 <criptotimsah2@gmail.com>
2026-03-09 14:43:31 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:43:30.318Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-09T11:43:32.552Z
+Commit: Fix CSS styling bug on mobile devices
